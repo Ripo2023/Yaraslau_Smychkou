fa9cd6ae3a7aa5f257ead7eaaa668ff15a2ffecd
User <yaraslau_smychkou@gmail.com>
2023-05-14 17:29:46 +0300

    MobileStore-Day-1 Implemented primary structure for IdentityProvider

---
+++ smychkou-yaraslau.identity-provider/MobileStore.IdentityProvider/MobileStore.IdentityProvider.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MobileStore.IdentityProvider.Common.Entities;
+
+namespace MobileStore.IdentityProvider.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
