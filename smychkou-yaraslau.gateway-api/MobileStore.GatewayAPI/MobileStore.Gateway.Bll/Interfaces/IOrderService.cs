a349b575246699bec17bcb8f8eb4ef6ada976edc
User <yaraslau_smychkou@gmail.com>
2023-05-15 17:47:12 +0300

    MobileStore-Day-2 Added possibility to store static files in gateway api

---
+++ smychkou-yaraslau.gateway-api/MobileStore.GatewayAPI/MobileStore.Gateway.Bll/Interfaces/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using MobileStore.Gateway.Common.RequestModels;
+using MobileStore.Gateway.Common.ResponseModels;
+
+namespace MobileStore.Gateway.Bll.Interfaces;
+
+public interface IOrderService
+{
+    Task<int> CreateOrder(int userId, CreateOrderRequestModel model);
+
+    Task<OrderDetails> GetOrderDetails(int orderId);
+
+    Task<IEnumerable<OrderPreview>> GetAllOrders(int userId);
+
+    Task UpdateOrder(UpdateOrderRequestModel model);
+}
---
+++ smychkou-yaraslau.gateway-api/MobileStore.GatewayAPI/MobileStore.Gateway.Bll/Services/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using MobileStore.Gateway.Bll.Interfaces;
+using MobileStore.Gateway.Common.RequestModels;
+using MobileStore.Gateway.Common.ResponseModels;
+using MobileStore.Gateway.Dal.Interfaces;
+
+namespace MobileStore.Gateway.Bll.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly IOrderRepository orderRepository;
+
+    public Task<int> CreateOrder(int userId, CreateOrderRequestModel model)
+    {
+        return orderRepository.CreateOrder(userId, model);
+    }
+
+    public Task<IEnumerable<OrderPreview>> GetAllOrders(int userId)
+    {
+        return orderRepository.GetAllOrders(userId);
+    }
+
+    public Task<OrderDetails> GetOrderDetails(int orderId)
+    {
+        return orderRepository.GetOrderDetails(orderId);
+    }
+
+    public Task UpdateOrder(UpdateOrderRequestModel model)
+    {
+        return orderRepository.UpdateOrder(model);
+    }
+}
